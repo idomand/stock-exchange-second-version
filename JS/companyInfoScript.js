8c07afc9d7f9ab3b4fb8cdd5b38e2bfd459c9234
ido mandelman <idomand@gmail.com>
2020-11-09 17:11:39 +0200

    milestone 2: done

---
+++ JS/companyInfoScript.js
@@ -0,0 +1,108 @@
+const idNames = [
+  "companyInfoDiv",
+  "companyLogo",
+  "companyName",
+  "stockPrice",
+  "stockPriceLine",
+  "stockPriceChange",
+  "companyDescription",
+];
+
+const [
+  companyInfoDiv,
+  companyLogo,
+  companyName,
+  stockPrice,
+  stockPriceLine,
+  stockPriceChange,
+  companyDescription,
+] = idNames.map((element) => document.getElementById(element));
+let url = window.location.href;
+let index = url.indexOf("=");
+let symbol = url.slice(index + 1);
+
+const baseUrl = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol}`;
+
+const findCompanyInfo = async () => {
+  toggleView(loader, "show");
+  let result = await fetchCompanyInfo(baseUrl);
+  await appendResults(result.profile);
+  await toggleView(loader, "hide");
+  await appendChart(symbol);
+};
+const fetchCompanyInfo = async (url) => {
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  return responseJson;
+};
+
+const appendResults = (object) => {
+  companyName.innerHTML = object.companyName;
+  toggleView(stockPriceLine, "show");
+  stockPrice.innerHTML = `${object.price} $`;
+  stockPrice.classList.add("stockPrice");
+  stockPriceChange.innerHTML = `${object.changes} $`;
+  companyDescription.innerHTML = object.description;
+  let logoImg = document.createElement("img");
+  logoImg.setAttribute("src", object.image);
+  companyLogo.appendChild(logoImg);
+  checkPriceChange(object.changes);
+};
+const checkPriceChange = (number) => {
+  if (number > 0) {
+    stockPriceChange.classList.add("positive");
+  } else if (number < 0) {
+    stockPriceChange.classList.add("negative");
+  } else {
+    stockPriceChange.innerHTML = "";
+  }
+};
+const appendChart = async (symbol) => {
+  let stockHistory = await fetch(
+    `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/historical-price-full/${symbol}?serietype=line`
+  );
+  let stockHistoryJson = await stockHistory.json();
+  createChart(stockHistoryJson.historical);
+};
+
+const createChart = (array) => {
+  let newArray = array.reverse();
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: newArray.map((element) => element.date),
+      datasets: [
+        {
+          label: "Stock Price over time ",
+          data: newArray.map((element) => element.close),
+          backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+          borderColor: ["rgba(153, 102, 255, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+};
+
+const toggleView = (element, order) => {
+  if (order === "show") {
+    element.classList.add("show");
+    element.classList.remove("hide");
+  } else if (order === "hide") {
+    element.classList.add("hide");
+    element.classList.remove("show");
+  }
+};
+findCompanyInfo();
--- JS/script.js
+++ JS/script.js
@@ -1,59 +1,71 @@
-(() => {
-  const idNames = ["myButton", "myInput", "resultList", "loader"];
+const idNames = [
+  "myButton",
+  "myInput",
+  "resultList",
+  "loader",
+  "companyInfoDiv",
+];
 
-  const [myButton, myInput, resultList, loader] = idNames.map((element) =>
-    document.getElementById(element)
-  );
+const [
+  myButton,
+  myInput,
+  resultList,
+  loader,
+  companyInfoDiv,
+] = idNames.map((element) => document.getElementById(element));
 
-  const baseUrl = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com`;
-  let userSearchInput = "";
-  let searchUrl = `/api/v3/search?query=${userSearchInput}&limit=10&exchange=NASDAQ`;
+const baseUrl = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com`;
+let userSearchInput = "";
+let searchUrl = `/api/v3/search?query=${userSearchInput}&limit=10&exchange=NASDAQ`;
 
-  myButton.addEventListener("click", () => {
-    freshStart();
-    toggleView(loader, "show");
-    findCompanies(myInput.value);
+myButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  freshStart();
+  toggleView(loader, "show");
+  findCompanies(myInput.value);
+});
+
+const findCompanies = async (searchInput) => {
+  userSearchInput = searchInput;
+  let foo = `${baseUrl}${searchUrl}`;
+  toggleView(loader, "show");
+  const results = await fetchFunc(foo);
+  showCompanies(results);
+};
+
+const fetchFunc = async (url) => {
+  let response = await fetch(url);
+  let responseJson = await response.json();
+  return responseJson;
+};
+
+const showCompanies = (array) => {
+  toggleView(loader, "hide");
+
+  let liElements = array.map((element) => {
+    const newLi = document.createElement("li");
+    const newATag = document.createElement("a");
+    newLi.appendChild(newATag);
+    newATag.setAttribute("href", `../company.html?symbol=${element.symbol}`);
+    newATag.setAttribute("target", "_blank");
+    newATag.innerHTML = `${element.name} (${element.symbol})`;
+    return newLi;
   });
+  liElements.forEach((li) => {
+    resultList.appendChild(li);
+  });
+};
 
-  const findCompanies = async (searchInput) => {
-    userSearchInput = searchInput;
-    let foo = `${baseUrl}${searchUrl}`;
-    toggleView(loader, "show");
-    const results = await fetchFunc(foo);
-    showCompanies(results);
-  };
+const freshStart = () => {
+  resultList.innerHTML = "";
+};
 
-  const fetchFunc = async (url) => {
-    let response = await fetch(url);
-    let responseJson = await response.json();
-    return responseJson;
-  };
-
-  const showCompanies = (array) => {
-    toggleView(loader, "hide");
-
-    array.forEach((element) => {
-      const newLi = document.createElement("li");
-      resultList.appendChild(newLi);
-      const newATag = document.createElement("a");
-      newLi.appendChild(newATag);
-      newATag.setAttribute("href", `../company.html?symbol=${element.symbol}`);
-      newATag.setAttribute("target", "_blank");
-      newATag.innerHTML = `${element.name} (${element.symbol})`;
-    });
-  };
-
-  const freshStart = () => {
-    resultList.innerHTML = "";
-  };
-
-  const toggleView = (element, order) => {
-    if (order === "show") {
-      element.classList.add("show");
-      element.classList.remove("hide");
-    } else if (order === "hide") {
-      element.classList.add("hide");
-      element.classList.remove("show");
-    }
-  };
-})();
+const toggleView = (element, order) => {
+  if (order === "show") {
+    element.classList.add("show");
+    element.classList.remove("hide");
+  } else if (order === "hide") {
+    element.classList.add("hide");
+    element.classList.remove("show");
+  }
+};
